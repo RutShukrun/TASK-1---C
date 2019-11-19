db0077b0c9d8795055dbcb6a45aaad38effe51d2
RutShukrun <57772853+RutShukrun@users.noreply.github.com>
2019-11-19 22:23:38 +0200

    Update basicMath.c

--- basicMath.c
+++ basicMath.c
@@ -1,1 +1,20 @@
 
+float add (float x, float y)
+{
+	return x+y;
+}
+	
+float sub (float x, float y)
+{
+	return x-y;
+}
+	
+float mul (float x, float y)
+{
+	return x*y;
+}
+
+float Div (float x, float y)	
+{
+	return x/y;
+}
